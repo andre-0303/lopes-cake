b26412e9b9bbb7a2b537da974e947f16bfa6bd58
Bandeira <bandeiradev.ti@gmail.com>
2025-09-05 19:47:38 -0300

    Finalizando alterações da feature

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,5 @@
+import { ShoppingCart } from "lucide-react";
+
 interface ProductCardProps {
   name: string;
   price: number;
@@ -10,14 +12,32 @@ export default function ProductCard({
   imageUrl,
 }: ProductCardProps) {
   return (
-    <div className="border rounded-lg p-4">
+    <div className="border rounded-lg p-4 w-[300px] flex gap-4 items-center shadow-sm hover:shadow-md transition">
+      {/* Imagem do produto */}
       <img
         src={imageUrl}
         alt={name}
-        className="w-full h-48 object-cover rounded-md"
+        className="w-24 h-24 object-cover rounded-md"
       />
-      <h2 className="text-lg font-semibold mt-2">{name}</h2>
-      <p className="text-pink-500 font-bold mt-1">R$ {price.toFixed(2)}</p>
+
+      {/* Informações e botão */}
+      <div className="flex flex-col justify-between flex-1 h-24">
+        <div>
+          <h2 className="text-md font-semibold text-start line-clamp-2">{name}</h2>
+          <p className="text-pink-500 font-bold mt-1 text-start">
+            R$ {price.toFixed(2)}
+          </p>
+        </div>
+
+        <button
+          className="mt-2 bg-pink-500 text-white text-sm font-semibold py-1 px-3 rounded hover:bg-pink-600 transition w-full"
+          onClick={() => alert(`Adicionou ${name} ao carrinho!`)}
+        >
+          <span className="flex items-center justify-center gap-2">
+            <ShoppingCart/>
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -33,7 +33,7 @@ export default function Produtos() {
   return (
     <div className="p-8">
       <Logo />
-      <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard
             key={product.id}
